bb00d16b037134d5c73445d8214304359a588c1f
50Start <74140514+50start@users.noreply.github.com>
2021-08-12 18:40:53 +0900

    Update auth.js

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -2,12 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAq1nOedD5tAVj1etvALzT_TzhtVYR8A_8",
-  authDomain: "fir-auth-24a19.firebaseapp.com",
-  projectId: "fir-auth-24a19",
-  storageBucket: "fir-auth-24a19.appspot.com",
-  messagingSenderId: "1039420048196",
-  appId: "1:1039420048196:web:b083e60775a2a04f0ccd11",
+  
 };
 
 firebase.initializeApp(firebaseConfig);

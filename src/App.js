bef83ff26dbd86dad9703def881d9f54067cd2c0
50start <seriebig18@outlook.jp>
2021-08-12 18:28:25 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ import {
 } from "@chakra-ui/react";
 import "./App.css";
 import { useState } from "react";
-import { singuprWithEmailAndPassword } from "./firebase/auth";
+import {
+  singuprWithEmailAndPassword,
+  singinrWithEmailAndPassword,
+  signout,
+} from "./firebase/auth";
 
 function App() {
   const [email, setEmail] = useState("");
@@ -30,7 +34,14 @@ function App() {
     //フォームのデフォルトの送信ボタンを押したときにデフォルトの挙動をprevent(防ぐ)
     //ページの再読み込み(リロードを防ぐ
     const user = await singuprWithEmailAndPassword(email, password);
-    console.log("User情報 :", user);
+    //auth.jsで　async await　を使っているのでここでも実装する　returnで成功したらuserが帰ってくる
+    console.log("登録User情報 :", user);
+  };
+
+  const signin = async (e) => {
+    e.preventDefault();
+    const user = await singinrWithEmailAndPassword(email, password);
+    console.log("サインインUser情報 :", user);
   };
 
   return (
@@ -39,6 +50,10 @@ function App() {
         <Heading as="h1" fontFamily="sans-serif">
           Auth動作確認
         </Heading>
+
+        <Box align="center" justify="center" color="#553C9A" fontSize="medium">
+          登録用フォーム
+        </Box>
 
         <form onSubmit={signup}>
           <Stack>
@@ -68,6 +83,58 @@ function App() {
             </Button>
           </Stack>
         </form>
+
+        <Box
+          align="center"
+          justify="center"
+          color="#97266D"
+          fontSize="medium"
+          mt={50}
+        >
+          ログインフォーム
+        </Box>
+        <form onSubmit={signin}>
+          <Stack>
+            <FormLabel>Email</FormLabel>
+            <Input
+              type="text"
+              value={email}
+              onChange={onChangeEmail}
+              h={20}
+              placeholder="email"
+            />
+            <FormLabel>password</FormLabel>
+            <Input
+              type="password"
+              value={password}
+              onChange={onChangePassword}
+              h={20}
+              placeholder="password"
+            />
+            <Button
+              type={"submit"}
+              backgroundColor="#319795"
+              color="white"
+              _hover={{ color: "black", backgroundColor: "#B2F5EA" }}
+            >
+              ログイン
+            </Button>
+          </Stack>
+        </form>
+        <Box>
+          <Stack>
+            <Button
+              type={"button"}
+              backgroundColor="#319795"
+              color="white"
+              _hover={{ color: "black", backgroundColor: "#B2F5EA" }}
+              mt={20}
+              onClick={signout}
+            >
+              ログアウト
+            </Button>
+          </Stack>
+        </Box>
       </Box>
     </Flex>
   );
--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -14,10 +14,12 @@ firebase.initializeApp(firebaseConfig);
 
 export const singuprWithEmailAndPassword = async (email, password) => {
   try {
+    //catchがあると失敗もあるのでtryを入れる
     const user = await firebase
-      .auth()
+      .auth() //成功したらuser情報が手に入る　変数userに代入される
+      //登録時ははこのメソッドを呼び出す
       .createUserWithEmailAndPassword(email, password);
-
+    //登録が成功すればcurrentUserにuser情報が入る　sendEmailVerification=> mail認証　メールが届いて登録処理が完了したら続きに行く
     await firebase.auth().currentUser.sendEmailVerification();
 
     alert("登録成功");
@@ -27,4 +29,31 @@ export const singuprWithEmailAndPassword = async (email, password) => {
     alert("登録失敗");
     console.log(error);
   }
+};
+
+export const singinrWithEmailAndPassword = async (email, password) => {
+  try {
+    const user = await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password); //await firebase.auth().currentUser.sendEmailVerification();
+    //サインインはこのメソッドを呼び出す　正しければuserにuser情報を受け取れる
+    //認証機能は登録時だけ
+
+    alert("サインイン成功");
+
+    return user;
+  } catch (error) {
+    //失敗したらerrorを受け取る
+    alert("サインイン失敗");
+    console.log(error);
+  }
+};
+
+export const signout = async (email, password) => {
+  //currentUserがあるかどうかのチェック
+  const user1 = await firebase.auth().currentUser;
+  console.log("サインアウト前 :", user1);
+  await firebase.auth().signOut();
+  const user2 = await firebase.auth().currentUser;
+  console.log("サインアウト後 :", user2);
 };
